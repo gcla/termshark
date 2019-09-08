eb0f3e7e6605fa7da1e61a3d65712f4b45117e2b
Graham Clark <grclark@gmail.com>
2019-09-08 10:41:19 -0400

    A start on cleanup up the notion of a source of packets.
    This distinguishes between files, fifos, pipes and interfaces. The logic in
    termshark.go was becoming cluttered and hard to reason with. This looks a bit
    java-ish... The interface is provided at the site of the structs, rather than
    by the user of the structs. This is intended to be used by the loader, and
    then clients of the loader can pass in conforming structs.

---
+++ pcap/source.go
@@ -0,0 +1,147 @@
+// Copyright 2019 Graham Clark. All rights reserved.  Use of this source
+// code is governed by the MIT license that can be found in the LICENSE
+// file.
+
+package pcap
+
+//======================================================================
+
+type IPacketSource interface {
+	Name() string
+	IsFile() bool
+	IsInterface() bool
+	IsFifo() bool
+	IsPipe() bool
+}
+
+//======================================================================
+
+func UIName(src IPacketSource) string {
+	if src.IsPipe() {
+		return "<stdin>"
+	} else {
+		return src.Name()
+	}
+}
+
+func CanRestart(src IPacketSource) bool {
+	return src.IsFile() || src.IsInterface()
+}
+
+//======================================================================
+
+type FileSource struct {
+	Filename string
+}
+
+var _ IPacketSource = FileSource{}
+
+func (p FileSource) Name() string {
+	return p.Filename
+}
+
+func (p FileSource) IsFile() bool {
+	return true
+}
+
+func (p FileSource) IsInterface() bool {
+	return false
+}
+
+func (p FileSource) IsFifo() bool {
+	return false
+}
+
+func (p FileSource) IsPipe() bool {
+	return false
+}
+
+//======================================================================
+
+type InterfaceSource struct {
+	Iface string
+}
+
+var _ IPacketSource = InterfaceSource{}
+
+func (p InterfaceSource) Name() string {
+	return p.Iface
+}
+
+func (p InterfaceSource) IsFile() bool {
+	return false
+}
+
+func (p InterfaceSource) IsInterface() bool {
+	return true
+}
+
+func (p InterfaceSource) IsFifo() bool {
+	return false
+}
+
+func (p InterfaceSource) IsPipe() bool {
+	return false
+}
+
+//======================================================================
+
+type FifoSource struct {
+	Filename string
+}
+
+var _ IPacketSource = FifoSource{}
+
+func (p FifoSource) Name() string {
+	return p.Filename
+}
+
+func (p FifoSource) IsFile() bool {
+	return false
+}
+
+func (p FifoSource) IsInterface() bool {
+	return false
+}
+
+func (p FifoSource) IsFifo() bool {
+	return true
+}
+
+func (p FifoSource) IsPipe() bool {
+	return false
+}
+
+//======================================================================
+
+type PipeSource struct {
+	Descriptor string
+}
+
+var _ IPacketSource = PipeSource{}
+
+func (p PipeSource) Name() string {
+	return p.Descriptor
+}
+
+func (p PipeSource) IsFile() bool {
+	return false
+}
+
+func (p PipeSource) IsInterface() bool {
+	return false
+}
+
+func (p PipeSource) IsFifo() bool {
+	return false
+}
+
+func (p PipeSource) IsPipe() bool {
+	return true
+}
+
+//======================================================================
+// Local Variables:
+// mode: Go
+// fill-column: 78
+// End:
