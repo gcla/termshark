81624944471e91ebfce4d254f95ba872694b7f09
Graham Clark <grclark@gmail.com>
2019-10-14 21:45:44 -0400

    Instead of using syscall, I should use the unix wrapper for Dup2.
    This covers the cases where it needs to be mapped to Dup3, so I don't
    need to.

--- system/fd.go
+++ system/fd.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/gcla/gowid"
 	"github.com/pkg/errors"
+	"golang.org/x/sys/unix"
 )
 
 //======================================================================
@@ -58,7 +59,7 @@ func MoveStdin() (int, error) {
 		return -1, err
 	}
 	if newstdin != 0 {
-		err = Dup2(newstdin, 0)
+		err = unix.Dup2(newstdin, 0)
 		if err != nil {
 			err = errors.WithStack(gowid.WithKVs(DupError, map[string]interface{}{
 				"descriptor": newstdin,
