a75ffca6832810a851f1ba03689b5c348d494989
Graham Clark <grclark@gmail.com>
2019-11-03 11:52:57 -0500

    A simple widget to issue callbacks when focus is lost or gained.
    Termshark uses this widget to make a small UX improvement in the new
    stream reassembly UI. Prior to this change, if you click in the list of
    stream payloads, termshark will open a small dialog to tell you it has
    selected packet X in the packet list (hidden until the stream UI is
    closed). The user then has to hit enter or click the close
    button. That's fine if the user is already navigating the list of stream
    chunks because they probably wouldn't click the mouse except to
    explicitly select a specific stream chunks. But if the stream UI focus
    is in e.g. the buttons at the bottom, the user might click with the
    mouse in the list of stream chunks just to select that section of the UI
    so that they can start to move up and down through the list with
    e.g. the cursor keys. In that case, it's annoying to have to dismiss the
    dialog telling the user they have selected a specific packet in the
    packet list. One option would be to make that dialog temporary without
    the need to be dismissed - like a message in a status bar. But another
    option is to distinguish the mouse clicks - (a) a click inside the list
    if the list already has focus, versus (b) a click inside the list when
    the list does not have focus. In case (b), termshark will then not
    display the dialog. This also gave the the chance to implement this
    feature which was requested by a gowid user.
    
    Termshark now sets a boolean in the state of the overall stream UI when
    the embedded list widget (for the stream chunks) gets focus and loses
    focus - this is done via the callbacks provided by this widget.

---
+++ widgets/trackfocus/trackfocus.go
@@ -0,0 +1,78 @@
+// Copyright 2019 Graham Clark. All rights reserved.  Use of this source
+// code is governed by the MIT license that can be found in the LICENSE
+// file.
+
+// Package trackfocus provides a widget that issues callbacks when a widget loses or gains the focus.
+package trackfocus
+
+import (
+	"github.com/gcla/gowid"
+)
+
+//======================================================================
+
+type Widget struct {
+	gowid.IWidget
+	init bool
+	last bool
+	cb   *gowid.Callbacks
+}
+
+func New(w gowid.IWidget) *Widget {
+	return &Widget{
+		IWidget: w,
+		cb:      gowid.NewCallbacks(),
+	}
+}
+
+// Markers to track the callbacks being added. These just need to be distinct
+// from other markers.
+type FocusLostCB struct{}
+type FocusGainedCB struct{}
+
+// Boilerplate to make the widget provide methods to add and remove callbacks.
+func (w *Widget) OnFocusLost(f gowid.IWidgetChangedCallback) {
+	gowid.AddWidgetCallback(w.cb, FocusLostCB{}, f)
+}
+
+func (w *Widget) RemoveOnFocusLost(f gowid.IIdentity) {
+	gowid.RemoveWidgetCallback(w.cb, FocusLostCB{}, f)
+}
+
+func (w *Widget) OnFocusGained(f gowid.IWidgetChangedCallback) {
+	gowid.AddWidgetCallback(w.cb, FocusGainedCB{}, f)
+}
+
+func (w *Widget) RemoveOnFocusGained(f gowid.IIdentity) {
+	gowid.RemoveWidgetCallback(w.cb, FocusGainedCB{}, f)
+}
+
+func (w *Widget) Render(size gowid.IRenderSize, focus gowid.Selector, app gowid.IApp) gowid.ICanvas {
+	res := w.IWidget.Render(size, focus, app)
+	if w.init && focus.Focus != w.last {
+		if focus.Focus {
+			gowid.RunWidgetCallbacks(w.cb, FocusGainedCB{}, app, w)
+		} else {
+			gowid.RunWidgetCallbacks(w.cb, FocusLostCB{}, app, w)
+		}
+	}
+	w.init = true
+	w.last = focus.Focus
+	return res
+}
+
+// Provide IComposite and ISettableComposite. This makes the widget cooperate with general
+// utilities that walk the widget hierarchy, like FocusPath().
+func (w *Widget) SubWidget() gowid.IWidget {
+	return w.IWidget
+}
+
+func (w *Widget) SetSubWidget(wi gowid.IWidget, app gowid.IApp) {
+	w.IWidget = wi
+}
+
+//======================================================================
+// Local Variables:
+// mode: Go
+// fill-column: 110
+// End:
