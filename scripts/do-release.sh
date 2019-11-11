58777c105744ae21d55faaa40a7a1e35445d3838
Graham Clark <grclark@gmail.com>
2019-11-10 20:02:13 -0500

    goreleaser updates for v2 publication.

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -6,4 +6,7 @@ git fetch --tags .
 export TERMSHARK_GIT_DESCRIBE="$(git describe --tags HEAD)"
 
 curl -sL https://git.io/goreleaser > /tmp/goreleaser.sh
-bash /tmp/goreleaser.sh --snapshot --skip-sign
+# testing
+# bash /tmp/goreleaser.sh --snapshot --skip-sign --rm-dist
+# release
+bash /tmp/goreleaser.sh --skip-sign --rm-dist
