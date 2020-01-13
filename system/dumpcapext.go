31e03c106b22688ae0db690816ed1196468c4c0a
Graham Clark <grclark@gmail.com>
2020-01-12 21:35:51 -0500

    A function to read from an interface with dumpcap then tshark
    If dumpcap fails, fall back to tshark. This is intended to provide the
    best of both worlds when reading a live capture on Unix - the more
    efficient dumpcap if possible, tshark if not.
    
    Note that one consequence of this is that on Unix, there are likely now
    two processes started for a live capture - termshark with
    TERMSHARK_CAPTURE_MODE=1, which then starts dumpcap as a child. This
    means capture termination needs to change to kill both processes.

---
+++ system/dumpcapext.go
@@ -0,0 +1,33 @@
+// Copyright 2019-2020 Graham Clark. All rights reserved.  Use of this source
+// code is governed by the MIT license that can be found in the LICENSE
+// file.
+
+package system
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// DumpcapExt will run dumpcap first, but if it fails, run tshark. Intended as
+// a special case to allow termshark -i <iface> to use dumpcap if possible,
+// but if it fails (e.g. iface==randpkt), fall back to tshark. dumpcap is more
+// efficient than tshark at just capturing, and will drop fewer packets, but
+// tshark supports extcap interfaces.
+func DumpcapExt(dumpcapBin string, tsharkBin string, args ...string) error {
+	var err error
+	dumpcapCmd := exec.Command(dumpcapBin, args...)
+	dumpcapCmd.Stdin = os.Stdin
+	dumpcapCmd.Stdout = os.Stdout
+	dumpcapCmd.Stderr = os.Stderr
+	if dumpcapCmd.Run() != nil {
+		var tshark string
+		tshark, err = exec.LookPath(tsharkBin)
+		if err == nil {
+			err = syscall.Exec(tshark, append([]string{tshark}, args...), os.Environ())
+		}
+	}
+
+	return err
+}
