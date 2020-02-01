a51195d03fa4ab5b01b8e3d0ea6ba7f7ef8d0142
Graham Clark <grclark@gmail.com>
2020-01-31 19:44:51 -0500

    An adaptor to allow a bounded table to be used with a scrollbar
    This widget provides ScrollLength() and ScrollPosition() APIs from the
    table.

---
+++ widgets/scrollabletable/scrollabletable.go
@@ -0,0 +1,52 @@
+// Copyright 2019-2020 Graham Clark. All rights reserved.  Use of this source
+// code is governed by the MIT license that can be found in the LICENSE
+// file.
+
+// Package scrollabletable makes a widget that some scrollbar interfaces
+// suitable for passing to withscrollbar.New()
+package scrollabletable
+
+import (
+	"github.com/gcla/gowid"
+	"github.com/gcla/gowid/widgets/table"
+	"github.com/gcla/termshark/v2/widgets/withscrollbar"
+)
+
+//======================================================================
+
+type IScrollableTable interface {
+	gowid.IWidget
+	withscrollbar.IScrollOneLine
+	withscrollbar.IScrollOnePage
+	CurrentRow() int
+	Model() table.IModel
+}
+
+// To implement withscrollbar.IScrollValues
+type Widget struct {
+	IScrollableTable
+}
+
+// makes a IScrollableTable suitable for passing to withscrollbar.New()
+var _ withscrollbar.IScrollSubWidget = Widget{}
+var _ withscrollbar.IScrollSubWidget = (*Widget)(nil)
+
+func New(t IScrollableTable) *Widget {
+	return &Widget{
+		IScrollableTable: t,
+	}
+}
+
+func (s Widget) ScrollLength() int {
+	return s.Model().(table.IBoundedModel).Rows()
+}
+
+func (s Widget) ScrollPosition() int {
+	return s.CurrentRow()
+}
+
+//======================================================================
+// Local Variables:
+// mode: Go
+// fill-column: 110
+// End:
