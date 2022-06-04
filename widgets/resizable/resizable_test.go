c840f4cd71fe243a4601c992e225cc00cd4e5396
Graham Clark <grclark@gmail.com>
2022-06-04 16:02:21 -0400

    Update copyrights

--- widgets/resizable/resizable_test.go
+++ widgets/resizable/resizable_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 Graham Clark. All rights reserved.  Use of this source code is governed by the MIT license
+// Copyright 2019-2022 Graham Clark. All rights reserved.  Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
 package resizable
